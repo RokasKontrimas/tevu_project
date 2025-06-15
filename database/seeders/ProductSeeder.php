c298b9f92e7dda7fd4d3a8a604f0046303ef35a1
RokasKontrimas <rokas.kontrimas123@gmail.com>
2025-06-15 22:14:53 +0300

    changed product prices

--- database/seeders/ProductSeeder.php
+++ database/seeders/ProductSeeder.php
@@ -15,11 +15,11 @@ class ProductSeeder extends Seeder
     {
         Product::insert([
             ['title' => 'Skerdiena', 'price' => 5.5, 'image' => 'images/broileris.jpg'],
-            ['title' => 'Sparneliai', 'price' => 3.90, 'image' => 'images/sparneliai.jpg'],
-            ['title' => 'Krūtinėlė', 'price' => 7.5, 'image' => 'images/krutinele.jpg'],
-            ['title' => 'Ketvirčiai', 'price' => 4.90, 'image' => 'images/Ketvirtis.jpg'],
+            ['title' => 'Sparneliai', 'price' => 4.2, 'image' => 'images/sparneliai.jpg'],
+            ['title' => 'Krūtinėlė', 'price' => 7.8, 'image' => 'images/krutinele.jpg'],
+            ['title' => 'Ketvirčiai', 'price' => 5.2, 'image' => 'images/Ketvirtis.jpg'],
             ['title' => 'Sriubos Rinkinys', 'price' => 2.5, 'image' => 'images/sriubos_rinkinys.jpg'],
-            ['title' => 'Subproduktai', 'price' => 3, 'image' => 'images/subproduktai.jpg']
+            ['title' => 'Subproduktai', 'price' => 3.3, 'image' => 'images/subproduktai.jpg']
 
         ]);
     }
